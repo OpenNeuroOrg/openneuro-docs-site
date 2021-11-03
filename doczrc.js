624fc5cd6e5d2f89a4e941caabd392d4f80c52c6
Nell Hardcastle <nell@dev-nell.com>
2021-11-03 15:00:26 -0700

    Initial configuration migrated from OpenNeuroOrg/openneuro repo

---
+++ doczrc.js
@@ -0,0 +1,28 @@
+export default {
+  title: 'OpenNeuro Documentation',
+  description: 'User and developer documentation for the OpenNeuro platform',
+  typescript: true,
+  menu: [
+    'Introduction',
+    'User Guide',
+    'GraphQL API',
+    'Command Line Interface',
+    'DataLad Service',
+    'Git Access',
+  ],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: 'docs.openneuro.org',
+        protocol: 'https',
+        hostname: 'docs.openneuro.org',
+      },
+    },
+  ],
+  files: [
+    'openneuro/docs/*.{md,mdx}',
+    'openneuro/packages/openneuro-cli/README.{md,mdx}',
+  ],
+  ignore: ['**/node_modules/**'],
+}
